0fca99b478950b764d2f26d8fe49b3566017aab0
Josef Larsson <josla972@student.liu.se>
2018-04-25 00:16:40 +0900

    Remove dhcp subdirs to disable it. Necessary?

--- profiles/dhcp/profile.mk
+++ profiles/dhcp/profile.mk
@@ -1,6 +1,5 @@
 
-WORLD_SUBDIRS_ZROUTER+=target/lib/libdhcp
-WORLD_SUBDIRS_ZROUTER+=target/sbin/dhcpd
-WORLD_SUBDIRS_ZROUTER+=target/sbin/dhclient
-WORLD_SUBDIRS_ZROUTER+=target/sbin/dhcrelay
-
+#WORLD_SUBDIRS_ZROUTER+=target/lib/libdhcp
+#WORLD_SUBDIRS_ZROUTER+=target/sbin/dhcpd
+#WORLD_SUBDIRS_ZROUTER+=target/sbin/dhclient
+#WORLD_SUBDIRS_ZROUTER+=target/sbin/dhcrelay
